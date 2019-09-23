77deab98cf7012bc8c5b3c4fb6dd6621d4394c7b
Brandon Rowe <brandonhrowe@gmail.com>
2019-09-23 14:34:12 -0400

    AuthorType added

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const schema = require("./schema/schema");
 
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,41 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require(graphql);
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt
+} = graphql;
+const _ = require("lodash");
+
+const books = [
+  { name: "To The Lighthouse", genre: "Fiction", id: "1" },
+  { name: "The Baron in the Trees", genre: "Fiction", id: "2" },
+  { name: "The Powerbroker", genre: "Non-Fiction", id: "3" },
+  { name: "Mrs. Dalloway", genre: "Fiction", id: "4" }
+];
+
+const authors = [
+  { name: "Virginia Woolf", age: 147, id: "1" },
+  { name: "Italo Calvino", age: 92, id: "2" },
+  { name: "Robert Caro", age: 80, id: "3" }
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -15,8 +44,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {}
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id });
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
+      }
     }
   }
 });
